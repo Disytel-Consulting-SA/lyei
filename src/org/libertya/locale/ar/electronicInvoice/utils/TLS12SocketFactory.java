76bc6ac48614588639213e04fe516935aec2e12a
fcristina <118191354+fcristina77@users.noreply.github.com>
2020-11-17 13:56:06 +0000

    Mensajes de error mas descriptivos.  Para TLS1.2 requiere java 8 o superior (sin esta revisión, al ejecutar con una version de java anterior directamente elevaba un NullPointerException)

--- src/org/libertya/locale/ar/electronicInvoice/utils/TLS12SocketFactory.java
+++ src/org/libertya/locale/ar/electronicInvoice/utils/TLS12SocketFactory.java
@@ -13,12 +13,17 @@ import org.apache.axis.components.net.JSSESocketFactory;
 
 public class TLS12SocketFactory extends JSSESocketFactory {
 	
+	protected static Integer javaMajorVersion = null;
+	
     public TLS12SocketFactory(Hashtable attributes) {
         super(attributes);
     }
 
     @Override
     public Socket create(String host, int port, StringBuffer otherHeaders, BooleanHolder useFullURL) throws Exception{
+    	// TLS1.2 es soportado a partir de Java 8
+        if (getVersion()!=null && getVersion() < 8)
+        	throw new RuntimeException("Se requiere Java 8 o superior para TLS1.2");
         Socket s = super.create(host, port, otherHeaders, useFullURL);
         ((SSLSocket)s).setEnabledProtocols(new String[] {/*"TLSv1.1",*/ "TLSv1.2"});
         return s;
@@ -33,5 +38,34 @@ public class TLS12SocketFactory extends JSSESocketFactory {
     		e.printStackTrace();
     	}
     };
+    
+    /** 
+     * Determina la major-version de Java 
+     * @return la versión correspondiente o null si no puede identificarlo
+     */
+    private static Integer getVersion() {
+    	// Cached?
+    	if (javaMajorVersion!=null) {
+    		return javaMajorVersion; 
+    	}
+    	try {
+	        String version = System.getProperty("java.version");
+	        // Java 8 or lower: 1.6.0_23, 1.7.0, 1.7.0_80, 1.8.0_211
+	        if(version.startsWith("1.")) {
+	            version = version.substring(2, 3);
+	        } else {
+	        	// Java 9 or higher: 9.0.1, 11.0.4, 12, 12.0.1
+	            int dot = version.indexOf(".");
+	            if(dot != -1) { 
+	            	version = version.substring(0, dot); 
+	            }
+	        } 
+	        javaMajorVersion = Integer.parseInt(version);
+	        return javaMajorVersion;
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+    }
     
 }
