cc4dbbbc4161f07a767f6895708166f9cc82bfd9
mncap <mncap@8919d973-c26e-4086-a362-5541261b4984>
2021-07-15 17:03:03 +0000

    Bug fix a postinstall

--- trunk/src/org/libertya/locale/ar/electronicInvoice/process/LYEIPostInstallUpgradeFrom_22.java
+++ trunk/src/org/libertya/locale/ar/electronicInvoice/process/LYEIPostInstallUpgradeFrom_22.java
@@ -7,7 +7,7 @@ import org.openXpertya.utils.JarHelper;
 public class LYEIPostInstallUpgradeFrom_22 extends PluginPostInstallProcess {
 
 	/** UID del Reporte de Comprobantes Registrados Electrónicos */
-	protected final static String LYEI_CCRE_JASPER_REPORT_UID = "LYEI-AD_Process-20210622152252429-454773";
+	protected final static String LYEI_CCRE_JASPER_REPORT_UID = "LYEI-AD_JasperReport-20210622152634885-593436";
 	protected final static String LYEI_CCRE_JASPER_REPORT_FILENAME = "LYEIConsultarComprobantesReport.jasper";
 	
 	@Override
