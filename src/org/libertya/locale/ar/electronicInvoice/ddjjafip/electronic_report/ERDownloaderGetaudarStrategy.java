cf498b0bfb5569d952cdacb88323994b83fd16b6
Julián Viejo <118192226+jviejo18@users.noreply.github.com>
2022-10-21 14:52:21 +0000

    -> Agregado procesamiento de archivos descargados para windows, contempla directorios con espacios, en linux hay problemas para la invocacion de algunos comandos como unzip cuando el path contiene espacios
    -> Pequeño refactor para evitar redundancia

--- src/org/libertya/locale/ar/electronicInvoice/ddjjafip/electronic_report/ERDownloaderGetaudarStrategy.java
+++ src/org/libertya/locale/ar/electronicInvoice/ddjjafip/electronic_report/ERDownloaderGetaudarStrategy.java
@@ -1,6 +1,12 @@
 package org.libertya.locale.ar.electronicInvoice.ddjjafip.electronic_report;
 
+import java.io.File;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import org.openXpertya.util.DB;
 
 public abstract class ERDownloaderGetaudarStrategy implements IElectronicReportDownloaderStrategy{
 	
@@ -22,4 +28,43 @@ public abstract class ERDownloaderGetaudarStrategy implements IElectronicReportD
 	protected abstract boolean isSpoolerStopped();
 
 	protected abstract boolean executeGetaudar(String baseDir, String fechaInicio, String fechaFin);
+	
+	protected int getRandomInt(int min, int max) {
+		int range = max - min;
+		return (int)(Math.random() * range) + min;
+	}
+	
+	protected ArrayList<String> getArchivosPresentacionFromDir(String dir) {
+		ArrayList<String> archivosPresentacionString = new ArrayList<String>();
+		String[] pathnames;
+        File f = new File(dir);
+        pathnames = f.list();
+        for (String pathname : pathnames) {
+        	String substring = pathname.substring(0, 5);
+        	if(substring.equalsIgnoreCase("F8011") || substring.equalsIgnoreCase("F8012")) {
+        		//establece el archivo con su ruta absoluta
+        		archivosPresentacionString.add(getFormattedPath(dir) + pathname);
+        	}
+        }
+		return archivosPresentacionString;
+	}
+	
+	protected abstract String getFormattedPath(String path);
+	
+	protected String getAD_PreferenceGetaudar() {
+		PreparedStatement pstmtMLA = 
+				DB.prepareStatement(	"SELECT value " +
+										"FROM ad_preference " +
+										"WHERE attribute = 'Getaudar'");
+		ResultSet rs = null;
+		String res = null;
+		try {
+		rs = pstmtMLA.executeQuery();
+		rs.next();
+		res = rs.getString(1);
+		} catch (SQLException e) {
+		e.printStackTrace();
+		}
+		return res;
+	}
 }
--- src/org/libertya/locale/ar/electronicInvoice/ddjjafip/electronic_report/ERDownloaderGetaudarWindowsStrategy.java
+++ src/org/libertya/locale/ar/electronicInvoice/ddjjafip/electronic_report/ERDownloaderGetaudarWindowsStrategy.java
@@ -1,5 +1,8 @@
 package org.libertya.locale.ar.electronicInvoice.ddjjafip.electronic_report;
 
+import java.io.File;
+import java.io.IOException;
+
 public class ERDownloaderGetaudarWindowsStrategy extends ERDownloaderGetaudarStrategy {
 
 	public ERDownloaderGetaudarWindowsStrategy() {
@@ -7,6 +10,54 @@ public class ERDownloaderGetaudarWindowsStrategy extends ERDownloaderGetaudarStr
 
 	@Override
 	public boolean downloadER(String fechaInicio, String fechaFin) {
+		//directorio base donde se encuentra la herramienta
+		String baseDir = getFormattedPath(getAD_PreferenceGetaudar());
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			//se invoca la herramienta externa para descargar afip.zip de impresora fiscal executeGetaudar(baseDir, fechaInicio, fechaFin);
+			
+			//Una vez obtenido el .zip desde la impresora fiscal es necesario
+			//guardar su contenido en un directorio particular, descomprimir y
+			//obtener los nombres de los archivos a presentar F8011... y F8012... (F8010 NO SE PRESENTA)
+			
+			//crear nuevo directorio especifico para la fecha de hoy
+			String newDir = fechaInicio + "_" + fechaFin + "_" + getRandomInt(1, 1000);
+			String newDirAbsolute = baseDir + newDir;
+			File newDirectory = new File(newDirAbsolute);
+			newDirectory.mkdirs();
+			
+			//mover afip.zip al nuevo directorio especifico
+			String[] mvCommand = {"cmd", "/c", "move " + addSurroundingQuotes(baseDir + "afip.zip") + " " + 
+															addSurroundingQuotes(newDirAbsolute)}; 
+			Process mv = runtime.exec(mvCommand);
+			mv.waitFor();
+			
+			//mover afip.85 al nuevo directorio especifico
+			String[] mvCommand2 = {"cmd", "/c", "move " + addSurroundingQuotes(baseDir + "afip.85") + " " + 
+															addSurroundingQuotes(newDirAbsolute)}; 
+			Process mv2 = runtime.exec(mvCommand2);
+			mv2.waitFor();
+			
+			//descomprimir zip y obtener archivos de interes
+			//cmd /c tar -xf "C:\path\to\afip.zip" -C "C:\path\to\"
+			String[] unzipCommand = {"cmd", "/c", "tar -xf " + addSurroundingQuotes(getFormattedPath(newDirAbsolute) + "afip.zip") + 
+					" " + "-C " + addSurroundingQuotes(newDirAbsolute)};
+			Process unzip = runtime.exec(unzipCommand);
+			//se debe esperar a que finalice el proceso, de lo contrario puede haber fallos
+			unzip.waitFor();
+			
+			//obtener a partir del nuevo directorio los archivos necesarios para presentacion y los seteamos
+			this.setArchivosPresentacion(getArchivosPresentacionFromDir(newDirAbsolute));
+			
+//			for(int i=0; i < this.archivosPresentacion .size() ; i++) {
+//				System.out.println("[ARCHIVO " + i +"] " + this.archivosPresentacion.get(i));
+//			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return true;
 	}
 
@@ -17,8 +68,40 @@ public class ERDownloaderGetaudarWindowsStrategy extends ERDownloaderGetaudarStr
 
 	@Override
 	protected boolean executeGetaudar(String baseDir, String fechaInicio, String fechaFin) {
+		//obtener parametros para ejecucion del script/herramienta externa
+		String tool = addSurroundingQuotes(getFormattedPath(baseDir) + "getaudar.exe");
+		String params = " -p 3 -i serial -a";
+		//ej: ubicacion/de/herramienta/getaudar -p 3 -i serial -a 200101 200331
+		String command = tool + params + " " + fechaInicio + " " + fechaFin;
+		//ejecutamos comando para obtener afip.zip
+		Runtime runtime = Runtime.getRuntime();
+		Process proc;
+		try {
+			proc = runtime.exec(command);
+			proc.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return true;
 	}
-
+	
+	//agrega comillas a una ruta para evitar problemas con paths que contengan espacios
+	private String addSurroundingQuotes(String path) {
+		if(!path.substring(0, 1).equalsIgnoreCase("\"")) {			
+			return "\"" + path + "\"";
+		}
+		return path;
+	}
+	
+	protected String getFormattedPath(String path) {
+		String res = path;
+		//si el path no termina con \ se le agrega
+		if(!path.substring(path.length() - 1).equals("\\") ) {
+			res = path + "\\";
+		}
+		return res;
+	}
 
 }
