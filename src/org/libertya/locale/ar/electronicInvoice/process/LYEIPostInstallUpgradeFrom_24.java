43aa1ba60f28fbd5d3ea7f9bd7aecff2a727ff23
dREHER <jorge.dreher@gmail.com>
2023-07-20 15:58:03 -0300

    Post install reporte jasper y binario del reporte de DDJJ presentadas

--- src/org/libertya/locale/ar/electronicInvoice/process/LYEIPostInstallUpgradeFrom_24.java
+++ src/org/libertya/locale/ar/electronicInvoice/process/LYEIPostInstallUpgradeFrom_24.java
@@ -1,21 +1,20 @@
 package org.libertya.locale.ar.electronicInvoice.process;
 
-import org.openXpertya.JasperReport.MJasperReport;
 import org.openXpertya.process.PluginPostInstallProcess;
-import org.openXpertya.utils.JarHelper;
 
 public class LYEIPostInstallUpgradeFrom_24 extends PluginPostInstallProcess {
 
 	/** UID del Reporte de DDJJ presentadas */
 	// TODO: verificar UID del jasper y actualizar la linea de abajo
-	protected final static String LYEI_DDJJ_JASPER_REPORT_UID = "LYEI-AD_JasperReport-20210622152634885-593436";
-	protected final static String LYEI_DDJJ_JASPER_REPORT_FILENAME = "DDJJ.jasper";
+	protected final static String LYEI_DDJJ_JASPER_REPORT_UID = "LYEI-AD_JasperReport-20230720153726659-059622";
+	protected final static String LYEI_DDJJ_JASPER_REPORT_FILENAME = "DDJJPresentadas.jasper";
 	
 	@Override
 	protected String doIt() throws Exception {
 		super.doIt();
 		
 		// Reporte de DDJJ presentadas
+		/**
 		MJasperReport
 			.updateBinaryData(
 					get_TrxName(),
@@ -25,6 +24,12 @@ public class LYEIPostInstallUpgradeFrom_24 extends PluginPostInstallProcess {
 							.readBinaryFromJar(
 									jarFileURL,
 									getBinaryFileURL(LYEI_DDJJ_JASPER_REPORT_FILENAME)));
+		
+		* Se trata de un reporte dinamico cuya query esta dentro del mismo jasper
+		* dREHER
+		*/
+		
+		updateReport(LYEI_DDJJ_JASPER_REPORT_UID, LYEI_DDJJ_JASPER_REPORT_FILENAME);
 		
 		return "";
 	}
