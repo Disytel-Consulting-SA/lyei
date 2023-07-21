1e149fd62da47faff6e863426dc474fe5b7535e9
dREHER <jorge.dreher@gmail.com>
2023-07-21 15:11:22 -0300

    Se reestructura el nombre de version del componente

--- src/org/libertya/locale/ar/electronicInvoice/process/LYEIPostInstallUpgradeFrom_23.java
+++ src/org/libertya/locale/ar/electronicInvoice/process/LYEIPostInstallUpgradeFrom_23.java
@@ -1,12 +1,17 @@
 package org.libertya.locale.ar.electronicInvoice.process;
 
+import org.openXpertya.JasperReport.MJasperReport;
 import org.openXpertya.process.PluginPostInstallProcess;
+import org.openXpertya.utils.JarHelper;
 
-public class LYEIPostInstallUpgradeFrom_24 extends PluginPostInstallProcess {
+public class LYEIPostInstallUpgradeFrom_23 extends PluginPostInstallProcess {
 
 	/** UID del Reporte de DDJJ presentadas */
-	// TODO: verificar UID del jasper y actualizar la linea de abajo
-	protected final static String LYEI_DDJJ_JASPER_REPORT_UID = "LYEI-AD_JasperReport-20230720153726659-059622";
+	// TODO: verificar UID del jasper y actualizar la linea de abajo 
+	protected final static String LYEI_DDJJ_JASPER_REPORT_UID = "LYEI-AD_Process-20230721101612939-083103";
+																
+																	// AL tratarse de un reporte adjunto, paso el UID del proceso y no del JasperReport
+																	// "LYEI-AD_JasperReport-20230721103037826-509907";
 	protected final static String LYEI_DDJJ_JASPER_REPORT_FILENAME = "DDJJPresentadas.jasper";
 	
 	@Override
